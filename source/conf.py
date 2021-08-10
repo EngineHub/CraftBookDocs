91ca7a18999c8a7828b438a98247e21ac7902eb5
Madeline Miller <mnmiller1@me.com>
2021-08-10 14:49:17 +1000

    Switch to custom EH YouTube embed plugin

--- source/conf.py
+++ source/conf.py
@@ -31,7 +31,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinxcontrib.yt',
+    'enginehub.sphinx_youtube',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
