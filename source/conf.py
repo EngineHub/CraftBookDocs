c75c278ce2cdab42fc2b38d5eccf76127f61121f
Madeline Miller <mnmiller1@me.com>
2021-08-16 20:50:53 +1000

    Uncomment the tex stuff

--- source/conf.py
+++ source/conf.py
@@ -210,7 +210,7 @@ htmlhelp_basename = 'CraftBookDocumentationdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-# latex_elements = {
+latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     # 'papersize': 'letterpaper',
 
@@ -222,16 +222,15 @@ htmlhelp_basename = 'CraftBookDocumentationdoc'
 
     # Latex figure (float) alignment
     # 'figure_align': 'htbp',
-# }
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-# latex_documents = [
-#     ('index', 'CraftBookDocumentation.tex', u'CraftBook Documentation Documentation',
-#      u'CraftBook Team', 'manual'),
-# ]
-latex_documents = []
+latex_documents = [
+    ('index', 'CraftBookDocumentation.tex', u'CraftBook Documentation Documentation',
+     u'CraftBook Team', 'manual'),
+]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
@@ -272,11 +271,11 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-# texinfo_documents = [
-#     ('index', 'CraftBookDocumentation', u'CraftBook Documentation Documentation',
-#      u'CraftBook Team', 'CraftBookDocumentation', 'One line description of project.',
-#      'Miscellaneous'),
-# ]
+texinfo_documents = [
+    ('index', 'CraftBookDocumentation', u'CraftBook Documentation Documentation',
+     u'CraftBook Team', 'CraftBookDocumentation', 'One line description of project.',
+     'Miscellaneous'),
+]
 
 # Documents to append as an appendix to all manuals.
 # texinfo_appendices = []
